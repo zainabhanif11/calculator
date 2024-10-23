5e866420ec31b2782076ca3f295cad54df93e095
Zainab Hanif <152397363+zainabhanif11@users.noreply.github.com>
2024-10-23 12:43:34 +0500

    Add files via upload

---
+++ calculator2/main.js
@@ -0,0 +1,23 @@
+const display = document.querySelector('.value');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach( (item) => {
+    item.onclick = () => {
+        try{
+            if(item.dataset.button === 'C'){
+                display.value = '';
+            } else if( item.dataset.button === 'CE') {
+                let string = string.substr(0 , string.length-1);
+        } else if(item.dataset.button === '=') {
+         if(display.value !== '') {
+            display.value = eval(display.value);
+         }
+        }else {
+            display.value += item.dataset.button;
+        }
+    }catch (err) {
+        display.value = 'Inavlid Entry';
+        setTimeout( () => (display.value = '') , 1000)
+    }
+}
+})
